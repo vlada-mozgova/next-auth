b6568d128b8be8a0ea415235b9783138282dfff3
vlada-mozgova <vladyslava.m@gmail.com>
2022-09-05 22:00:27 +0300

    add project

---
+++ components/layout/layout.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import MainNavigation from "./main-navigation";
+
+const Layout = ({ children }) => {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main>{children}</main>
+    </Fragment>
+  );
+};
+
+export default Layout;
