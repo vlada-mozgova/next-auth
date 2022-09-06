69ffdc03a184a1f39ff038e7790c805709062909
vlada-mozgova <vladyslava.m@gmail.com>
2022-09-06 12:57:16 +0300

    add session control

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,10 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-
 import classes from "./main-navigation.module.css";
 
 const MainNavigation = () => {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -12,15 +14,21 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
